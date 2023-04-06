8aa57923350fd883126ce2a45ab9ab34d11627b7
KindlyDrow <116948580+KindlyDrow@users.noreply.github.com>
2023-04-06 12:46:20 +0300

    SynhronizeState

--- Assets/Scripts/UI/TutorialUI.cs
+++ Assets/Scripts/UI/TutorialUI.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _readyText;
+    [SerializeField] private TextMeshProUGUI _notReadyText;
+
     private void Start()
     {
         GameManager.Instance.OnStateChange += GameManager_OnStateChange;
+        GameManager.Instance.OnLocalPlayerReadyChanged += GameManager_OnLocalPlayerReady;
         Show();
+        ShowNotReady();
+    }
+
+    private void GameManager_OnLocalPlayerReady(object sender, System.EventArgs e)
+    {
+        if (GameManager.Instance.IsLocalPlayerReady())
+        {
+            ShowReady();
+        } else
+        {
+            ShowNotReady();
+        }
     }
 
     private void GameManager_OnStateChange(object sender, System.EventArgs e)
@@ -26,5 +43,17 @@ public class TutorialUI : MonoBehaviour
     private void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private void ShowReady()
+    {
+        _readyText.gameObject.SetActive(true);
+        _notReadyText.gameObject.SetActive(false);
+    }
+
+    private void ShowNotReady()
+    {
+        _readyText.gameObject.SetActive(false);
+        _notReadyText.gameObject.SetActive(true);
     }
 }
